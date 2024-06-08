899bd8eec585fc6ef3851893d024f5199ef3a0b5
slo0ey <rurchi1206@gmail.com>
2024-06-09 00:55:24 +0900

    ESLint, prettier 설정 완료

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,8 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-ally/recommended',
+    'plugin:prettier/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
